34f6144b319f0179b8b3e9d5b192a17881ecddb6
josephtmartin <goldglovejo13@gmail.com>
2021-03-27 16:06:16 -0500

    changed padding

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ import TypingEffect from '../components/TypingEffect';
 export default function Home() {
   return (
     <div>
-      <h2 style={{ paddingTop: '100px', paddingBottom: '400px', color: '#fafafa' }}>Home</h2>
+      <h2 className='home-title' style={{ paddingTop: '100px', paddingBottom: '200px', color: '#fafafa' }}>Home</h2>
       <TypingEffect/>
     </div>
   );
