fb2710f80eff8bbe9875a57d379cae6c4499a45e
joe <goldglovejo13@gmail.com>
2021-04-25 15:45:57 -0500

    changed wording on the bio

--- src/views/Bio.js
+++ src/views/Bio.js
@@ -9,19 +9,18 @@ export default function Bio() {
         the Nashville, TN area. I am finishing my Bachelor's degree in Audio Production and
         beginning a Masters of Fine Arts in Recording Arts and Technology at Middle Tennessee State
         University. I have a deep passion for pushing faders and making music, especially in studio
-        settings. I also work as a songwriter and artist and have been writing for 6-7 years. I
-        believe hard work, dedication, and letting the music tell its own story is the best way to
-        make great music happen.
+        settings. I also work as a songwriter and artist and have been writing for 6-7 years. I believe
+        in hard work, dedication, and letting the song tell it's own story.
       </p>
 
       <b />
 
       <p className='skills-para'>
-        I am proficient in Pro Tools and have experience on large format consoles, including the
-        API Vision and the SSL Duality. I have tracked bands on both boards, as well as mixed on the
-        Vision, various smaller SSL consoles, and in the box. My focus is in tracking live
-        recording instruments and mixing to let the realness of those tracks shine for themselves.
-        My genre experience in recording includes Rock, Pop-Rock, Country, and Folk.
+        I am proficient in Pro Tools and have experience on large format consoles and on smaller interfaces, including the
+        API Vision and the SSL Duality. I have tracked bands on both consoles, as well as mixed on the
+        Vision, various smaller SSL consoles, and in the box. My focus has been in tracking live
+        instruments and mixing to let the authenticity of the tracks shine for themselves.
+        My genre experience in recording includes Rock, Pop, Country, and Folk.
       </p>
     </div>
   );
