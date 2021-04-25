59e6586719b70c77ee41419f65828614e2806b24
joe <goldglovejo13@gmail.com>
2021-04-25 16:00:37 -0500

    changed font size of the contact email

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <h2 className='facebook-link'><a href="https://www.facebook.com/DaleShackOfficial" target="_blank" rel="noreferrer">Facebook</a></h2>
       <h2 className='instagram-link'><a href="https://www.instagram.com/daleshackofficial/" target="_blank" rel="noreferrer">Instagram</a></h2>
       <h2 className='email'>Email:</h2>
-      <h2 className='email'>daleshack28@gmail.com</h2>
+      <h3 className='email'>daleshack28@gmail.com</h3>
     </div>
   );
 }
