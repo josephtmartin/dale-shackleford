6e043ad8b6bfa4e86e125fcd162efab749dad775
josephtmartin <goldglovejo13@gmail.com>
2022-01-22 11:27:20 -0600

    Saving recent

--- src/components/TypingEffect/index.js
+++ src/components/TypingEffect/index.js
@@ -2,28 +2,32 @@ import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 
 const ReactTypingEffectDemo = () => (
-    <>
-      <ReactTypingEffect
-        text={['Hello, My Name Is Dale Shackleford', 'I am a producer, engineer, and musician.']}
-        typingDelay= '4000'
-        speed= '100'
-        eraseDelay= '1000'
-        eraseSpeed= '50'
-        cursorRenderer={(cursor) => <h1 style={{ color: 'white' }}>{cursor}</h1>}
-        displayTextRenderer={(text, i) => (
-            <h1>
-              {text.split('').map((char) => {
-                const key = `${i}`;
-                return (
-                  <span className='typewriter' key={key} style={{ color: 'white', fontFamily: "'Special Elite', cursive" }}>
-                    {char}
-                  </span>
-                );
-              })}
-            </h1>
-        )}
-      />
-    </>
+  <>
+    <ReactTypingEffect
+      text={['Hello, My Name Is Dale Shackleford', 'I am a producer, engineer, and musician.']}
+      typingDelay='4000'
+      speed='100'
+      eraseDelay='1000'
+      eraseSpeed='50'
+      cursorRenderer={(cursor) => <h1 style={{ color: 'white' }}>{cursor}</h1>}
+      displayTextRenderer={(text, i) => (
+        <h1>
+          {text.split('').map((char) => {
+            const key = `${i}`;
+            return (
+              <span
+                className='typewriter'
+                key={key}
+                style={{ color: 'white', fontFamily: "'Special Elite', cursive" }}
+              >
+                {char}
+              </span>
+            );
+          })}
+        </h1>
+      )}
+    />
+  </>
 );
 
 export default ReactTypingEffectDemo;
