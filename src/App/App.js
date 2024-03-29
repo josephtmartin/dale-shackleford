7363754ce29d8dbb5df7c8edf3852f5fa1b03d87
josephtmartin <goldglovejo13@gmail.com>
2024-03-29 11:16:37 -0500

    updated react version

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
-//added test comment
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Navbar from '../components/Navbar';
-import Routes from '../helpers/Routes';
+// import Routes from '../helpers/Routes';
 // import ResponsiveDrawer from '../components/DemoNavBar';
 import SideNav from '../components/SideNav';
+import Bio from '../views/Bio';
+import Contact from '../views/Contact';
+import Home from '../views/Home';
+import MyMusic from '../views/MyMusic';
+import NotFound from '../views/NotFound';
+import Produced from '../views/Produced';
+import Research from '../views/Research';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <Router>
-          <SideNav />
-          <Routes />
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <SideNav />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            exact
+            path='/bio'
+            element={<Bio />}
+          />
+          <Route
+            exact
+            path='/production-engineering'
+            element={<Produced />}
+          />
+          <Route
+            exact
+            path='/my-music'
+            element={<MyMusic />}
+          />
+          <Route
+            exact
+            path='/contact'
+            element={<Contact />}
+          />
+          <Route
+            exact
+            path='/research'
+            element={<Research />}
+          />
+          <Route element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
