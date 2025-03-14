c65fc2d4364a9350aa5daef10f1af2f2e1a0331a
Joseph Martin <41244241+josephtmartin@users.noreply.github.com>
2025-03-13 19:08:23 -0500

    added pdf viewer

--- src/views/Research.js
+++ src/views/Research.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 export default function Research() {
+  const [showPDF, setShowPDF] = useState(false);
+
   return (
     <div className='research-page'>
       <h1 className='research-title' style={{ paddingBottom: '100px', color: '#fafafa' }}>Research</h1>
@@ -12,7 +16,23 @@ export default function Research() {
       various Native American cultures have influenced modern society. In terms of music specifically, there are very few descriptions and very little documentation of early Native American
       songs as the technology for recording was not invented until 1877. This thesis is merely a first step in introducing the idea that American Indigenous music has had an influence on
       popular forms of American music, such as Delta Blues. The written portion of this thesis has been done in conjunction with supporting audio musical compositions (many of which are original for this project) to assist indemonstration.</p>
-      <iframe title='thesis' src='files\Dante Networking and System Design at MTSU_Shackleford.pdf' width='100%' height='600px'></iframe>
+      {/* Toggle Button */}
+      <button
+        className='btn btn-primary'
+        onClick={() => setShowPDF(!showPDF)}
+      >
+        {showPDF ? 'Hide PDF' : 'Show PDF'}
+      </button>
+
+      {/* Collapsible PDF Viewer */}
+      <div className={`collapse ${showPDF ? 'show' : ''}`} style={{ marginTop: '20px' }}>
+        <iframe
+          title='thesis'
+          src='/Dante Networking and System Design at MTSU_Shackleford.pdf'
+          width='100%'
+          height='600px'
+        ></iframe>
+      </div>
     </div>
   );
 }
